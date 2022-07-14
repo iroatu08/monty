3bf9511125392cf57307a08f3b237f6af8dc2cac
iroatu08 <iroatu08@gmail.com>
2022-07-15 00:13:47 +0100

    fixes

--- rotr.c
+++ rotr.c
@@ -1,31 +1,16 @@
 #include "monty.h"
 
 /**
-* rotr - rotates the last node of a stack_t stack
-* @stack: stack head
-* @line_count: line count
-*
-* Return: void
-*/
-void rotr(stack_t **stack, unsigned int line_count)
+ * rotr - rotates the stack to the bottom.
+ * @stack: double pointer to the begining of the linked list
+ * @line_number: script line number
+ *
+ * Return: void
+ */
+void rotr(stack_t **stack, unsigned int line_number)
 {
-	stack_t *bottom;
-	stack_t *prev;
+	(void)line_number;
 
-	(void) line_count;
-	if (!stack || !*stack || !(*stack)->next)
-		return;
-
-	bottom = *stack;
-
-	while (bottom->next)
-		bottom = bottom->next;
-
-	prev = bottom->prev;
-	bottom->next = *stack;
-	bottom->prev = NULL;
-	prev->next = NULL;
-	(*stack)->prev = bottom;
-	*stack = bottom;
+	if (*stack)
+		*stack = (*stack)->prev;
 }
-
